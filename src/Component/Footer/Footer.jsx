11bdcccec1d95b15479ec82c7f6b07c6d2fda71e
shuence <Shubhampitekar98@gmail.com>
2023-07-08 20:32:48 +0530

    Just warning solving

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import "./Footer.css"
 
-import { Container,Row,Col, ListGroupItem, ListGroup } from 'reactstrap'
-import { Link } from 'react-router-dom'
-import logo from "../../assets/images/logo.png"
+import { Container,Row,Col } from 'reactstrap'
+
 
 const Footer = () => {
   return (
